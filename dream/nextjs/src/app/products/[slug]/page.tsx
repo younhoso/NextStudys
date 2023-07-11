b80a700e6b5723383ca835adfd05dd4464eba258
younhoso <thdbsgh3443@naver.com>
2023-07-12 06:50:02 +0900

    update: 페이지 미리 생성기(SSG)

--- dream/nextjs/src/app/products/[slug]/page.tsx
+++ dream/nextjs/src/app/products/[slug]/page.tsx
@@ -6,4 +6,11 @@ type Props = {
 
 export default function PantsPage({params}: Props) {
   return <h1>{params.slug} 바지 제품 설명</h1>
-}
+};
+
+export function generateStaticParams() {
+  const products = ['pants', 'skirt'];
+  return products.map((product) => ({
+    slug: product
+  }))
+};
