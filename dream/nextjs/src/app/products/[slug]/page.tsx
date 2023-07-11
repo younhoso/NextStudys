b8bc15ba41556537ca18b1ab2a75150d1e44c5f6
younhoso <thdbsgh3443@naver.com>
2023-07-12 07:16:58 +0900

    update: notfund 페이지 작업

--- dream/nextjs/src/app/products/[slug]/page.tsx
+++ dream/nextjs/src/app/products/[slug]/page.tsx
@@ -1,3 +1,5 @@
+import { notFound } from 'next/navigation';
+
 type Props = {
   params: {
     slug: string;
@@ -5,6 +7,10 @@ type Props = {
 };
 
 export default function PantsPage({params}: Props) {
+  if(params.slug === 'nothing'){
+    notFound();
+  }
+  
   return <h1>{params.slug} 바지 제품 설명</h1>
 };
 
