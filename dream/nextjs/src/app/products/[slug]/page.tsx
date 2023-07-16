9eab89f0c9c2863744975bc49dc68182def62b70
younhoso <thdbsgh3443@naver.com>
2023-07-17 07:03:58 +0900

    update: SEO 메타데이타 링크 수정

--- dream/nextjs/src/app/products/[slug]/page.tsx
+++ dream/nextjs/src/app/products/[slug]/page.tsx
@@ -6,6 +6,13 @@ type Props = {
   };
 };
 
+
+export function generateMetadata({params}: Props){
+  return {
+    title: `제품의 이름: ${params.slug}`
+  }
+}
+
 export default function PantsPage({params}: Props) {
   if(params.slug === 'nothing'){
     notFound();
