da50b4b52f849f51fe1ea19ac6b3d7f88a0cfa0f
younhoso <thdbsgh3443@naver.com>
2023-07-16 11:03:45 +0900

    update: 레이아웃 파트

--- dream/nextjs/src/app/layout.tsx
+++ dream/nextjs/src/app/layout.tsx
@@ -1,6 +1,7 @@
 import './globals.css'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
+import styles from './layout.module.css'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -16,7 +17,16 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <header className={styles.header}>
+          <h1>Demo Note</h1>
+          <nav className={styles.nav}>
+            <a href="#0">Contact</a>
+            <a href="#0">About</a>
+          </nav>
+        </header>
+        {children}
+      </body>
     </html>
   )
 }
