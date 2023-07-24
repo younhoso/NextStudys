5907dc89424594073a9adb3434cab82f1e50f6ba
younhoso <thdbsgh3443@kakao.com>
2023-07-24 09:13:26 +0900

    update: ssg 렌더링 방식

--- dream/nextjs/src/app/products/page.tsx
+++ dream/nextjs/src/app/products/page.tsx
@@ -1,14 +1,14 @@
 import { getProducts } from "@/service/products";
 import Link from "next/link";
 
-export default function ProductsPage() {
-  const products = getProducts();
+export default async function ProductsPage() {
+  const products = await getProducts();
   return (
     <>
       <h1>제품 소개 페이지!</h1>
       <ul>
         { products.map((product, index) => (<li key={index}>
-          <Link href={`products/${product}`}>{product}</Link>
+          <Link href={`products/${product.id}`}>{product.name}</Link>
         </li>))}
       </ul>
     </>
--- dream/nextjs/src/service/products.ts
+++ dream/nextjs/src/service/products.ts
@@ -1,7 +1,19 @@
-export function getProducts() {
-  return ['shirt', 'pants', 'skirt', 'shoes', 'dress'];
+import { promises } from "fs";
+import path from "path";
+
+export type Product = {
+  id: string;
+  name: string;
+  price: number;
+}
+
+export async function getProducts(): Promise<Product[]> {
+  const filePath = path.join(process.cwd(), 'data', 'products.json');
+  const data = await promises.readFile(filePath, 'utf-8');
+  return JSON.parse(data);
 };
 
-export function getProduct(id: string) {
-  return 'shirt';
+export async function getProduct(id: string): Promise<Product | undefined> {
+  const products = await getProducts();
+  return products.find(item => item.id === id);
 }
